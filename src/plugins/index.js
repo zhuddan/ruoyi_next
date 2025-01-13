3f011e8a491c25e81b377359fc2ab752ad0666ff
zhuddan <yobaci@163.com>
2025-01-13 15:10:19 +0800

    $auth

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -1,13 +1,12 @@
-import auth from './auth'
+// import auth from './auth'
 import cache from './cache'
 import modal from './modal'
-import tab from './tab'
 
 export default function installPlugins(app) {
   // 页签操作
-  app.config.globalProperties.$tab = tab
+  // app.config.globalProperties.$tab = tab
   // 认证对象
-  app.config.globalProperties.$auth = auth
+  // app.config.globalProperties.$auth = auth
   // 缓存对象
   app.config.globalProperties.$cache = cache
   // 模态框对象
