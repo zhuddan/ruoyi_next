f4b812672c01583c951e7955c2211e20caef958a
zhuddan <yobaci@163.com>
2025-01-13 12:04:09 +0800

    fix

--- vite/plugins/index.js
+++ vite/plugins/index.js
@@ -2,13 +2,11 @@ import vue from '@vitejs/plugin-vue'
 
 import createAutoImport from './auto-import'
 import createCompression from './compression'
-import createSetupExtend from './setup-extend'
 import createSvgIcon from './svg-icon'
 
 export default function createVitePlugins(viteEnv, isBuild = false) {
   const vitePlugins = [vue()]
   vitePlugins.push(createAutoImport())
-  vitePlugins.push(createSetupExtend())
   vitePlugins.push(createSvgIcon(isBuild))
   isBuild && vitePlugins.push(...createCompression(viteEnv))
   return vitePlugins
