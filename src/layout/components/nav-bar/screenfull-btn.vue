beb728723ce5d6662ff3dc36a6912fde58df7072
zhuddan <yobaci@163.com>
2025-01-14 16:14:05 +0800

    fix: 删除不必要的全局组件

--- src/layout/components/nav-bar/screenfull-btn.vue
+++ src/layout/components/nav-bar/screenfull-btn.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { useFullscreen } from '@vueuse/core'
 
-const { isFullscreen, enter, exit, toggle } = useFullscreen()
+const { isFullscreen, toggle } = useFullscreen()
 </script>
 
 <template>
@@ -9,14 +9,3 @@ const { isFullscreen, enter, exit, toggle } = useFullscreen()
     <svg-icon :icon-class="isFullscreen ? 'exit-fullscreen' : 'fullscreen'" @click="toggle" />
   </div>
 </template>
-
-<style lang='scss' scoped>
-.screenfull-svg {
-  display: inline-block;
-  cursor: pointer;
-  fill: #5a5e66;
-  width: 20px;
-  height: 20px;
-  vertical-align: 10px;
-}
-</style>
